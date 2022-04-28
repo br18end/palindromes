dcfa48b271ec223d7d99c0eb7d6ce6e5f1165c6f
Brenda Castro <br18end@gmail.com>
2022-04-28 16:58:55 -0700

    Add project files.

---
+++ palindromes/Program.cs
@@ -0,0 +1,39 @@
+﻿using Diacritics.Extensions;
+
+Console.WriteLine("Enter a word");
+string? word = Console.ReadLine();
+
+if (word is null)
+{
+    return;
+}
+
+Console.WriteLine($"Your word: {word}");
+
+word = word.Trim().RemoveDiacritics();
+string[] specialCharacters = { ".", ",", "?" };
+
+for (int i = 0; i < word.Length; i++)
+{
+    for (int j = 0; j < specialCharacters.Length; j++)
+    {
+        word = word.Replace(specialCharacters[j], string.Empty);
+    }
+}
+
+Console.WriteLine($"Your word without special characters: {word}");
+
+char[] array = word.ToCharArray();
+Array.Reverse(array);
+
+string wordReverse = new string(array);
+//wordReverse = wordReverse.Trim(specialCharacters);
+
+Console.WriteLine($"Reversed word: {wordReverse}");
+
+var isPalyndrome = word.ToLower().Equals(wordReverse.ToLower());
+
+if (isPalyndrome)
+    Console.WriteLine("The word introduced is a palindrome");
+else
+    Console.WriteLine("The word introduced is NOT a palindrome ");
